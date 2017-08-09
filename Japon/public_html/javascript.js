b10f5b6589f1df1ba2e3bc2ca96d31704eae20e1
boul <boul@boul-HP-ProBook-450-G3>
2017-08-09 11:03:47 +0200

    formulaire : creation du champ suppl

--- Japon/public_html/javascript.js
+++ Japon/public_html/javascript.js
@@ -1,3 +1,15 @@
+function mail() {
+    var champ = document.createElement("input");
+    champ.name = "email1";
+    champ.type = "text";
+    var bloc = document.createElement("p");
+    var insert = document.getElementById("insert");
+    bloc.appendChild(champ);
+
+    insert.insertBefore(bloc, insert.lastChild);
+}
+
+
 
 
 
