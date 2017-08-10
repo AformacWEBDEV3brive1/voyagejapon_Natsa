c539afbd3809942fdb9f23345cc32287b4b02aea
boul <boul@boul-HP-ProBook-450-G3>
2017-08-10 08:14:05 +0200

    footer modifié

--- Japon/public_html/javascript.js
+++ Japon/public_html/javascript.js
@@ -1,11 +1,16 @@
+var repeat = 0;
+if (repeat < 4) {
+    mail();
+} else
+    ;
 function mail() {
+    repeat = repeat + 1;
     var champ = document.createElement("input");
     champ.name = "email1";
     champ.type = "text";
     var bloc = document.createElement("p");
     var insert = document.getElementById("insert");
     bloc.appendChild(champ);
-
     insert.insertBefore(bloc, insert.lastChild);
 }
 
