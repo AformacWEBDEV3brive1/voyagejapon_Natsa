66565515a716868a89b2e05d494669feceb4ef70
nathaniel <nathaniel@AOC>
2017-08-10 10:03:40 +0200

    Tether  installer et cookies terminé

--- Japon/public_html/javascript.js
+++ Japon/public_html/javascript.js
@@ -1,17 +1,20 @@
+
 function mail() {
+    var nbr = parseInt(document.getElementById('insert').childNodes.length);
+    if(nbr < 6){
     var champ = document.createElement("input");
     champ.name = "email1";
     champ.type = "text";
     var bloc = document.createElement("p");
+    
     var insert = document.getElementById("insert");
     bloc.appendChild(champ);
-
     insert.insertBefore(bloc, insert.lastChild);
+    }
+    else{
+        
+    }
 }
-
-
-
-
-
-
-
+function popin(){
+    alert("Attention , il y a des cookies sur cette page!");
+}
